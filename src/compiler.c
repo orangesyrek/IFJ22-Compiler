46befa39cc3fb7d4a9c7ac279cbc94e6eea59368
Roytak <xjanot04@fit.vutbr.cz>
2022-10-10 18:02:52 +0200

    reorganisation

---
+++ src/compiler.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "compiler.h"
+
+int
+compiler_ctx_new(struct compiler_ctx **ctx)
+{
+	*ctx = calloc(1, sizeof **ctx);
+	if (!*ctx) {
+		return 1;
+	}
+
+	return 0;
+}
+
+void
+compiler_ctx_destroy(struct compiler_ctx *ctx)
+{
+	free(ctx);
+}
