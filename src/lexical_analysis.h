a284ee15c7fd159bba8d3eec084a253ea2847df6
Roytak <xjanot04@fit.vutbr.cz>
2022-10-13 21:06:59 +0200

    basic lex analysis struct

--- src/lexical_analysis.h
+++ src/lexical_analysis.h
@@ -2,46 +2,57 @@
 #define LEX_ANALYSIS_H
 
 typedef enum {
-	FUN_ID = 0,	/* foo */
-	STR_LIT,	/* "ab" */
-	ASSIGNMENT,	/* = */
-	INT_LIT,	/* 42 */
-	DECIMAL_LIT,	/* 42.5, 42e+10,42.5E-2 */
-	PARAM_TYPE,	/* int, ?int */
-	VAR,		/* $x */
-	L_PAR,		/* ( */
-	R_PAR,		/* ) */
-	SEMICOLON,	/* ; */
-	L_CURLY,	/* { */
-	R_CURLY,	/* } */
-	REL_IDENTICAL,	/* === */
-	REL_NOT_IDENTIKAL, /* !== */
-	REL_LESS,	/* < */
-	REL_LESS_EQ,	/* <= */
-	REL_GREATER,	/* > */
-	REL_GREATER_EQ,	/* >= */
-	PLUS,	/* + */
-	MINUS,	/* - */
-	ASTERISK,	/* * */
-	SLASH,	/* / */
-	DOT,	/* . */
-	COMMA,	/* , */
-	COLON,	/* : */
-	EXCLAMATION_MARK,	/* ! */
-	NL,	/* \n */
-	KEYWORD_IF,	/* if */
-	KEYWORD_ELSE,	/* else */
-	KEYWORD_FUNCTION,	/* function */
-	KEYWORD_NULL,	/* null */
-	KEYWORD_RETURN,	/* return */
-	KEYWORD_VOID,	/* void */
-	KEYWORD_WHILE,	/* while */
-	KEYWORD_DECLARE, /* declare */
-	KEYWORD_STRICT_TYPES, /* strict_types */
-	COMMENT,	/* */
-	PROLOG_END, /* ?> */
-	LEX_EOF,	/* EOF */
+    FUN_ID = 0,    /* foo */
+    STR_LIT,    /* "ab" */
+    ASSIGNMENT,    /* = */
+    INT_LIT,    /* 42 */
+    DECIMAL_LIT,    /* 42.5, 42e+10,42.5E-2 */
+    VAR,        /* $x */
+    L_PAR,        /* ( */
+    R_PAR,        /* ) */
+    SEMICOLON,    /* ; */
+    L_CURLY,    /* { */
+    R_CURLY,    /* } */
+    REL_IDENTICAL,    /* === */
+    REL_LESS,    /* < */
+    REL_LESS_EQ,    /* <= */
+    REL_GREATER,    /* > */
+    REL_GREATER_EQ,    /* >= */
+    PLUS,    /* + */
+    MINUS,    /* - */
+    ASTERISK,    /* * */
+    SLASH,    /* / */
+    DOT,    /* . */
+    COMMA,    /* , */
+    COLON,    /* : */
+    PARAM_TYPE,    /* int, ?int */
+    COMMENT,    /* */
+    PROLOG_START,	/* <?php */
+    PROLOG_END, /* ?> */
+    LEX_EOF,    /* EOF */
+    KEYWORD_IF,    /* if */
+    KEYWORD_ELSE,    /* else */
+    KEYWORD_FUNCTION,    /* function */
+    KEYWORD_NULL,    /* null */
+    KEYWORD_RETURN,    /* return */
+    KEYWORD_VOID,    /* void */
+    KEYWORD_WHILE,    /* while */
+    KEYWORD_DECLARE, /* declare */
+    KEYWORD_STRICT_TYPES, /* strict_types */
 } lex_types;
+
+struct lexeme {
+    lex_types type;
+    char *id;
+
+    union {
+        int int_val;
+        double flt_val;
+        char *str_val;
+    } value;
+
+    unsigned int symtab_index;
+};
 
 void print_ctx();
 
