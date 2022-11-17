c82c3bb19e246346021482915dd833ce5e5a5cdb
afrosamuraj99 <tvalik@seznam.cz>
2022-11-17 13:30:49 +0100

    skelet of symtab

--- src/symtab.c
+++ src/symtab.c
@@ -1,1 +1,10 @@
 #include "symtab.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void symtabInit(struct bs_tree *t){
+    if(t != NULL){
+      t = NULL;
+    }
+}
+  
--- src/symtab.h
+++ src/symtab.h
@@ -3,34 +3,49 @@
 
 #include <stdint.h>
 
+typedef enum{
+	STRING,
+	INT,
+	FLOAT,
+	BOOL,
+	NULL, // idk if not rename
+	VOID,
+	QSTRING,
+	QINT,
+	QFLOAT
+} type;
+
 struct bs_tree {
-	uint32_t key;
+	char *key;
 	void *data;
 	struct bs_tree *left;
 	struct bs_tree *right;
 };
 
+struct function_data{
+	type return_type;
+	int is_defined;
+	int *params;
+	int paramcount;
+};
+
+struct variable_data{
+	int is_init;
+	type data_type;
+	int is_used;
+};
 struct bs_data{
 	uint32_t id;
 
 	int is_function;
 	union {
-		struct function_data;
-		struct variable_data;
+		struct function_data fdata;
+		struct variable_data vdata;
 	} data;
 
 };
 
-struct function_data{
-type return_type;
-int is_defined;
-//list of parameters
-};
 
-struct variable_data{
-int is_init;
-type data_type;
-int is_used;
-}
+void symtabInit(struct bs_tree *t);
 
 #endif
